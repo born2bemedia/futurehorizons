211b1bfa5714dd8fbf05901d3875d3da4171e5bb
SergWebAcceptance <serg.web@acceptance.ltd>
2024-07-25 18:43:48 +0300

    Added Parallax effect to homepage

--- src/app/(home)/_components/HomeHero.jsx
+++ src/app/(home)/_components/HomeHero.jsx
@@ -4,46 +4,49 @@ import "@/styles/home/homeHero.scss";
 import { fadeInUp } from "@/app/utils/animations";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { Parallax } from 'react-parallax';
 
 const HomeHero = () => {
   return (
-    <section className="home-hero">
-      <div className="_container">
-        <div className="home-hero__body">
-          <motion.h2
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeInUp}
-          >
-            Brand name
-          </motion.h2>
-          <motion.h1
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeInUp}
-          >
-            Data-driven <br />
-            Marketing Solutions
-          </motion.h1>
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeInUp}
-            className="buttons-wrap"
-          >
-            <Link href="#" className="bordered-button">
-              Explore services
-            </Link>
-            <Link href="#" className="orange-button">
-              Get a proposal
-            </Link>
-          </motion.div>
+    <Parallax bgImage="/images/home/homeHero.webp" strength={300}>
+      <section className="home-hero">
+        <div className="_container">
+          <div className="home-hero__body">
+            <motion.h2
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={fadeInUp}
+            >
+              Brand name
+            </motion.h2>
+            <motion.h1
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={fadeInUp}
+            >
+              Data-driven <br />
+              Marketing Solutions
+            </motion.h1>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={fadeInUp}
+              className="buttons-wrap"
+            >
+              <Link href="#" className="bordered-button">
+                Explore services
+              </Link>
+              <Link href="#" className="orange-button">
+                Get a proposal
+              </Link>
+            </motion.div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Parallax>
   );
 };
 
