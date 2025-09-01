90a46636bebfed7dca3cc33aeb3f41d7c3552428
Daniil <dan.developer@born2be.media>
2025-09-01 18:08:56 +0300

    [feature/new-primary-lng]: New Primary Lng (#2)

--- src/middleware.js
+++ src/middleware.js
@@ -7,18 +7,10 @@ export default createMiddleware({
   defaultLocale: 'bg'
 });
 
-// only applies this middleware to files in the app directory
 export const config = {
   matcher: [
-    // Enable a redirect to a matching locale at the root
     '/',
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
     '/(de|en|it|bg)/:path*',
-
-    // Enable redirects that add missing locales
-    // (e.g. `/pathnames` -> `/en/pathnames`)
     '/((?!_next|_vercel|.*\\..*).*)'
   ]
 };
--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,7 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
 export const locales = ['bg', 'en', 'de', 'it'];
-export const localePrefix = 'as-needed';
+export const localePrefix = 'always';
 
 export const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales, localePrefix });
